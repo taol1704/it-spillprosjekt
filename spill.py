836669eca46fa63b1980feb996d6f68824e26a13
taol1704 <taraldfo@gmail.com>
2023-11-16 13:25:19 +0100

    first commit

---
+++ spill.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+# 1. oppsett
+pygame.init()
+BREDDE = 600
+HOYDE = 600
+FPS = 60
+vindu = pygame.display.set_mode((BREDDE, HOYDE))
+klokke = pygame.time.Clock()
+
+while True:
+    # 2. Håndter input
+    for hendelse in pygame.event.get():
+        if hendelse.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+        
+    # 3. oppgrader spill
+    # 4. Tegn
+
+    pygame.display.flip()
+    klokke.tick(FPS)
